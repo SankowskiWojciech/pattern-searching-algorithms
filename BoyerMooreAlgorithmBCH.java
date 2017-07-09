8e27726b36b47f80e4e1ae6195a3bed6bac8f69e
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-07-09 09:58:50 +0200

    implementation of Boyer-Moore string search algorithm

--- BoyerMooreAlgorithmBCH.java
+++ BoyerMooreAlgorithmBCH.java
@@ -2,21 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoyerMooreAlgorithmBCH {
-    private int[] last;
-    private final String data;
-    private final String pattern;
 
     private final int FIRST_CHAR_CODE = 0;
     private final int LAST_CHAR_CODE = 255;
 
-
-    public BoyerMooreAlgorithmBCH(final String data, final String pattern) {
-        this.data = data.toUpperCase();
-        this.pattern = pattern.toUpperCase();
-        setLast();
-    }
-
-    public List<Integer> search() {
+    public List<Integer> search(final String data, final String pattern) {
+        int[] last = setLast(pattern);
         List<Integer> foundIndexes = new ArrayList<>();
         int i = 0;
         int j;
@@ -31,12 +22,13 @@ public class BoyerMooreAlgorithmBCH {
         return foundIndexes.size() > 0 ? foundIndexes : null;
     }
 
-    private void setLast() {
-        last = new int[LAST_CHAR_CODE - FIRST_CHAR_CODE + 1];
+    private int[] setLast(final String pattern) {
+        int[] last = new int[LAST_CHAR_CODE - FIRST_CHAR_CODE + 1];
         for(int i = 0; i < last.length; i++)
             last[i] = -1;
         for(int i = 0; i < pattern.length(); i++) {
             last[pattern.charAt(i) - FIRST_CHAR_CODE] = i;
         }
+        return last;
     }
 }
